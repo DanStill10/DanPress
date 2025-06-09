0b6a426055ca72f2399a10910af5335796933339
Daniel Still <daniel.still@sprydigital.com>
2025-06-08 21:36:54 -0500

    ignore added, along with errors resolved regarding bud dev builds

---
+++ wp-content/themes/dan-press-components/bud.config.mjs
@@ -0,0 +1,33 @@
+// @ts-check
+
+import BudSass from '@roots/bud-sass';
+import BudReact from '@roots/bud-react';
+import BudSWC from '@roots/bud-swc';
+
+/**
+ * Build configuration
+ * @param {import('@roots/bud').Bud} app
+ */
+export default async (app) => {
+  app
+    /** Set paths to match your theme's structure */
+    .setPath({
+      '@src': 'resources',
+      '@dist': 'public',
+    })
+    /** Define entrypoints using the correct paths */
+    .entry({
+      app: ['@src/scripts/app.js', '@src/styles/app.scss'],
+    })
+
+    .use([BudSass, BudReact, BudSWC])
+    .watch(['**/*.php']);
+
+  if (app.isDevelopment) {
+    app.dev
+      .setProxyUrl('http://dan-still-wordpress-site.lndo.site')
+      .setUrl('http://localhost:3000');
+  }
+
+  return app;
+};
