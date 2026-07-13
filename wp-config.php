f01c0ceeb798a680c8294b110c5edf976f3a1e71
Daniel Still <daniel.still@sprydigital.com>
2026-07-13 16:28:30 -0500

    mysql first railway deployment reqs

--- wp-config.php
+++ wp-config.php
@@ -20,19 +20,19 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress' );
+define( 'DB_NAME', getenv('DB_NAME') ?: 'wordpress' );
 
 /** Database username */
-define( 'DB_USER', 'wordpress' );
+define( 'DB_USER', getenv('DB_USER') ?: 'wordpress' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'wordpress' );
+define( 'DB_PASSWORD', getenv('DB_PASSWORD') ?: 'wordpress' );
 
 /** Database hostname */
-define( 'DB_HOST', 'database' );
+define( 'DB_HOST', getenv('DB_HOST') ?: 'database' );
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
+define( 'DB_CHARSET', 'utf8mb4' );
 
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
