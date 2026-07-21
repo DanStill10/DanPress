3375ad7447f1c176464fb044c22dc01ae72bfe7f
Daniel Still <daniel.still@sprydigital.com>
2026-07-20 21:42:42 -0500

    feat(value-props): ACF repeater 3-column layout
    - ACF field group (repeater: title, subtitle, description)
    - Equal-width CSS grid columns, flex column cards
    - Responsive: collapses to single column on tablet

--- wp-content/themes/dan-press-components/template-parts/value-props.php
+++ wp-content/themes/dan-press-components/template-parts/value-props.php
@@ -2,16 +2,42 @@
 /**
  * Template Part: Value Propositions (3-Column)
  *
- * Three key value propositions displayed side by side.
- * Content managed via ACF repeater field.
+ * Displays key value props in equal columns.
+ * Content managed via ACF repeater field on the front page.
  *
  * @package Dan Press
  */
 
-// TODO: Build in feature/value-props branch
+if ( ! function_exists( 'have_rows' ) ) {
+    return;
+}
+
+if ( ! have_rows( 'value_props' ) ) {
+    return;
+}
 ?>
+
 <section class="dsd-value-props" id="value-props">
     <div class="container">
-        <p class="dsd-value-props__placeholder">[Value propositions will appear here]</p>
+        <div class="dsd-value-props-grid">
+            <?php while ( have_rows( 'value_props' ) ) : the_row();
+
+                $title       = get_sub_field( 'value_prop_title' );
+                $subtitle    = get_sub_field( 'value_prop_subtitle' );
+                $description = get_sub_field( 'value_prop_description' );
+            ?>
+                <div class="dsd-value-prop">
+                    <h3 class="dsd-value-prop__title"><?php echo esc_html( $title ); ?></h3>
+
+                    <?php if ( $subtitle ) : ?>
+                        <p class="dsd-value-prop__subtitle"><?php echo esc_html( $subtitle ); ?></p>
+                    <?php endif; ?>
+
+                    <?php if ( $description ) : ?>
+                        <p class="dsd-value-prop__desc"><?php echo esc_html( $description ); ?></p>
+                    <?php endif; ?>
+                </div>
+            <?php endwhile; ?>
+        </div>
     </div>
 </section>
