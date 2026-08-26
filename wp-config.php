4b3f616d83a7d9c805f0db47801ed57513028fc7
Daniel Still <daniel.still@sprydigital.com>
2026-08-25 21:32:04 -0500

    railwaiy ssl handling settled in php config

--- wp-config.php
+++ wp-config.php
@@ -1,4 +1,10 @@
 <?php
+//Railway reverse proxy SSL detection
+define('FORCE_SSL_ADMIN', true);
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+    $_SERVER['HTTPS'] = 'on';
+}
+
 /**
  * The base configuration for WordPress
  *
